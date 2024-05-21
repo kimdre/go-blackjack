77931213fd1de5f884272121bbce3df1570cb0f5
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-05-21 19:43:41 +0200

    Finish base game
    Signed-off-by: Kim Oliver Drechsel <kim@drechsel.xyz>

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -1,12 +1,11 @@
 package blackjack
 
 import (
-	"fmt"
 	"math/rand"
 )
 
 type Card struct {
-	Color string
+	Suit  string
 	Name  string
 	Value []uint8
 }
@@ -15,30 +14,30 @@ type CardDeck struct {
 	Cards []Card
 }
 
-var CardColors = []string{"Kreuz", "Pik", "Herz", "Karo"}
+var CardSuits = []string{"Clubs", "Diamonds", "Hearts", "Spades"}
 
 var CardTypes = map[string][]uint8{
-	"zwei":   {2},
-	"drei":   {3},
-	"vier":   {4},
-	"fünf":   {5},
-	"sechs":  {6},
-	"sieben": {7},
-	"acht":   {8},
-	"neun":   {9},
-	"zehn":   {10},
-	"bube":   {10},
-	"dame":   {10},
-	"könig":  {10},
-	"ass":    {1, 10},
+	"Two":   {2},
+	"Three": {3},
+	"Four":  {4},
+	"Five":  {5},
+	"Dix":   {6},
+	"Seven": {7},
+	"Eight": {8},
+	"Nine":  {9},
+	"Ten":   {10},
+	"Jacl":  {10},
+	"Queen": {10},
+	"King":  {10},
+	"Ace":   {10}, // {1, 10},
 }
 
 func (deck *CardDeck) generate() {
-	for _, color := range CardColors {
+	for _, suit := range CardSuits {
 		for key, value := range CardTypes {
 			card := Card{
-				Color: color,
-				Name: key,
+				Suit:  suit,
+				Name:  key,
 				Value: value,
 			}
 			deck.Cards = append(deck.Cards, card)
@@ -52,32 +51,33 @@ func (deck *CardDeck) Shuffle() {
 	rand.Shuffle(len(deck.Cards), func(i, j int) {
 		deck.Cards[i], deck.Cards[j] = deck.Cards[j], deck.Cards[i]
 	})
-
-	fmt.Println(deck.Cards)
 }
 
 type Rules struct {
-	MaxPoints uint8
-	Rounds uint8
+	MaxPoints   uint8
+	TotalRounds uint8
 }
 
 type Round struct {
-	Points uint8
-	History []Card
-	Deck CardDeck
+	Points   uint8
+	CardHand []Card
+	Deck     CardDeck
 }
 
 type Game struct {
-	Rules Rules 
-	Round uint
+	Rules  Rules
 	Losses uint
-	Wins uint
+	Wins   uint
 	Rounds []Round
 }
 
-func (r *Round) NewRound() {
-	r.Points = 0
-	r.Deck.Shuffle()
+func NewRound() *Round {
+	round := &Round{
+		Points: 0,
+		Deck:   CardDeck{},
+	}
+	round.Deck.Shuffle()
+	return round
 }
 
 func (r *Round) DrawCard() Card {
@@ -86,9 +86,11 @@ func (r *Round) DrawCard() Card {
 	return card
 }
 
-func (g *Game) NewGame() {
-	g.Rules.MaxPoints = 21
-	g.Rules.Rounds = 6
-	g.Losses = 0
-	g.Wins = 0
+func NewGame() *Game {
+	return &Game{
+		Rules: Rules{
+			MaxPoints:   21,
+			TotalRounds: 6,
+		},
+	}
 }
--- main.go
+++ main.go
@@ -1,10 +1,113 @@
 package main
 
-import "github.com/kimdre/blackjack/blackjack"
+import (
+	"fmt"
+	"time"
+
+	"github.com/kimdre/blackjack/blackjack"
+	"github.com/kimdre/blackjack/utils"
+)
 
 func main() {
-deck := blackjack.CardDeck{}
-deck.Shuffle()
+	game := blackjack.NewGame()
 
+	for {
+		player := blackjack.NewRound()
+		dealer := blackjack.NewRound()
 
+		var roundEnd = false
+		var playerRound = true
+
+		for i := 0; i < 2; i++ {
+			dealerCard := player.DrawCard()
+			dealer.Points += dealerCard.Value[0]
+			dealer.CardHand = append(dealer.CardHand, dealerCard)
+
+			card := player.DrawCard()
+			player.Points += card.Value[0]
+			player.CardHand = append(player.CardHand, card)
+		}
+
+		for {
+			utils.ClearTerminal()
+			fmt.Printf("Round # %v of %v\nCards left in Deck: %v\nWins: %v, Losses: %v\n\n", len(game.Rounds)+1, game.Rules.TotalRounds, len(player.Deck.Cards), game.Wins, game.Losses)
+			if playerRound {
+				fmt.Println("Dealer cards:", dealer.CardHand[0], "???")
+				fmt.Println("Dealer points:", dealer.CardHand[0].Value)
+			} else {
+				fmt.Println("Dealer cards:", dealer.CardHand)
+				fmt.Println("Dealer points:", dealer.Points)
+			}
+			fmt.Println()
+			fmt.Println("Your cards:", player.CardHand)
+			fmt.Println("Your points:", player.Points)
+			fmt.Println()
+
+			if player.Points <= game.Rules.MaxPoints && dealer.Points > game.Rules.MaxPoints {
+				fmt.Println("You win.")
+				fmt.Println()
+				game.Wins += 1
+				roundEnd = true
+			} else if player.Points >= game.Rules.MaxPoints && dealer.Points >= game.Rules.MaxPoints {
+				fmt.Println("Draw.")
+				fmt.Println()
+				roundEnd = true
+			} else if player.Points > game.Rules.MaxPoints && dealer.Points <= game.Rules.MaxPoints {
+				fmt.Println("You loose.")
+				fmt.Println()
+				game.Losses += 1
+				roundEnd = true
+			} else if player.Points < dealer.Points && dealer.Points <= game.Rules.MaxPoints && !playerRound {
+				fmt.Println("You loose.")
+				fmt.Println()
+				game.Losses += 1
+				roundEnd = true
+			}
+
+			if !roundEnd && !playerRound {
+				fmt.Println("Dealer draws new card...")
+				time.Sleep(1 * time.Second)
+				dealerCard := player.DrawCard()
+				dealer.Points += dealerCard.Value[0]
+				dealer.CardHand = append(dealer.CardHand, dealerCard)
+			}
+
+			if !roundEnd && playerRound {
+				var choice string
+				fmt.Print("Draw another card? [y/n] ")
+				fmt.Scan(&choice)
+
+				switch choice {
+				case "y":
+					card := player.DrawCard()
+					player.Points += card.Value[0]
+					player.CardHand = append(player.CardHand, card)
+				case "n":
+					playerRound = false
+				default:
+					return
+				}
+			}
+
+			if roundEnd {
+				if len(game.Rounds)+1 >= int(game.Rules.TotalRounds) {
+					utils.ClearTerminal()
+					fmt.Println("You finished the game")
+					fmt.Printf("Wins: %v, Losses: %v\nTotal Rounds: %v\n", game.Wins, game.Losses, len(game.Rounds)+1)
+					return
+				}
+
+				var newRound string
+				fmt.Print("New round? [y/n] ")
+				fmt.Scan(&newRound)
+
+				if newRound != "y" {
+					return
+				}
+				break
+			}
+		}
+
+		game.Rounds = append(game.Rounds, *player)
+	}
 }
